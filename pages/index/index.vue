110601aef5f95bd70fbc5b08e79635f414174bc9
wangsw <wangsw@ushareit.com>
2020-02-26 18:59:38 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view><web-view src="https://blog.webkong.cn"></web-view></view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		};
+	},
+	onLoad() {},
+	methods: {},
+	onShareAppMessage(res) {
+		return {
+			title: 'WebKong的网络日志',
+			path: '/pages/index/index',
+			imageUrl: 'https://blog.webkong.cn/images/common/head.jpg'
+		};
+	}
+};
+</script>
+
+<style></style>
